791e489aa6ce6a7ffd1536c107c2a060b57e13a4
1 <a40104@naver.com>
2025-03-31 14:52:37 +0900

    Practice 5-1

---
+++ Main.java
@@ -0,0 +1,21 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
+
+  import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2, num3;
+
+        System.out.print("숫자1를 입력하시오: ");
+        num1 = sc.nextInt();
+        System.out.print("숫자2를 입력하시오: ");
+        num2 = sc.nextInt();
+        System.out.print("숫자3를 입력하시오: ");
+        num3 = sc.nextInt();
+
+        System.out.println(num1-num2-num3);
+    }
+}
